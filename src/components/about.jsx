57f884aeb14977b4d4fa784421a5b66a80f89042
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-02-29 12:13:22 -0300

    feat: finished about responsive

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [showMore, setShowMore] = useState(false);
   const [ref, inView] = useInView({
     triggerOnce: false,
-    threshold: 0.5,
+    threshold: 0,
   });
 
   const handleToggleShowMore = () => {
@@ -17,16 +17,26 @@ const About = () => {
   return (
     <div
       id="about"
-      className="flex flex-row bg-[#01020a] py-40 justify-center gap-32 px-20"
+      className="flex flex-col md:flex-row bg-[#01020a] py-40 justify-center gap-32 px-20"
     >
+      {/* Parte da imagem */}
       <div
-        className={`flex flex-col w-6/12 text-start gap-8 transition-opacity ease-in-out duration-1000 ${
+        className={`flex items-center w-full md:w-4/12 relative overflow-hidden md:order-first transition-opacity ease-in-out duration-1000 ${
+          inView ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <div className="absolute inset-0 bg-gradient-to-t from-[#010008] via-transparent to-transparent rounded-lg"></div>
+        <Img src={Photo} alt="Foto de Lucas Campos" className="rounded-lg" />
+      </div>
+
+      <div
+        className={`flex flex-col w-full md:w-6/12 text-start gap-8 justify-center items-center transition-opacity ease-in-out duration-1000 ${
           inView ? "opacity-100" : "opacity-0"
         }`}
         ref={ref}
       >
         <h1 className="text-gray-200 text-4xl font-bold">Sobre</h1>
-        <p className="text-gray-200 text-lg">
+        <p className="text-gray-200 md:text-lg md:p-0 p-2 text-justify md:text-start text-md md:w-auto w-80">
           Oi! sou o Lucas. Desenvolvedor Frontend e graduado em Segurança da
           Informação. Minha paixão é desenvolvimento web e experiência do
           usuário, juntando as duas coisas, consigo formar ótimas experiências
@@ -34,7 +44,7 @@ const About = () => {
           Me considero uma pessoa que aprende rapidamente, capaz de apreender
           novos conceitos com facilidade e excelente capacidade de demonstrá-los
           na prática.
-          <br /> De forma resumida, essas são as técnologias que uso
+          <br /> De forma resumida, essas são as tecnologias que uso
           diariamente: JavaScript, TypeScript, React.js, Next.js, Tailwindcss,
           Node.js e PostgreSQL.
         </p>
@@ -42,10 +52,10 @@ const About = () => {
           className="text-gray-300 font-bold rounded-sm bg-purple-900 w-60 cursor-pointer hover:bg-purple-950 transition-all duration-500"
           onClick={handleToggleShowMore}
         >
-          {showMore ? "Esconda esse texto de volta!" : "Saiba mais sobre..."}
+          {showMore ? "Esconda o texto abaixo!" : "Saiba mais..."}
         </button>
         <p
-          className={`text-gray-200 text-start text-lg transition-opacity ease-in-out duration-500 ${
+          className={`text-gray-200  md:text-lg text-justify md:text-start md:p-0 p-2 text-md md:w-auto w-80 transition-opacity ease-in-out duration-500 ${
             showMore ? "opacity-100" : "opacity-0"
           }`}
         >
@@ -65,11 +75,6 @@ const About = () => {
           programação oferece e ansioso para continuar explorando novos
           horizontes neste universo.
         </p>
-      </div>
-
-      <div className="flex items-center w-3/12 relative overflow-hidden">
-        <div className="absolute inset-0 bg-gradient-to-t from-[#010008] via-transparent to-transparent rounded-lg"></div>
-        <Img src={Photo} alt="Foto de Lucas Campos" className="rounded-lg " />
       </div>
     </div>
   );
