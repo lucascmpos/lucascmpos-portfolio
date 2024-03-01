24a1e13ca9e29fe909b68761b0ec0b11b7f09659
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-03-01 02:39:09 -0300

    fix: fixing 4k exp component responsiveness

--- src/components/exp.jsx
+++ src/components/exp.jsx
@@ -44,9 +44,9 @@ const Experience = () => {
           software, metodologias ágeis e AWS Cloud. <br />
           Atuei ativamente na implementação do desenvolvimento e testes
           integrados de componentes do Storybook do Livelo App, e também
-          participando de práticas ágeis, com Scrum e Kanban. <br />
-          Essa experiência ampliou minhas habilidades e consolidou meu
-          comprometimento com a excelência no desenvolvimento de software.
+          participando de práticas ágeis, com Scrum e Kanban. Essa experiência
+          ampliou minhas habilidades e consolidou meu comprometimento com a
+          excelência no desenvolvimento de software.
           <br />
           <br />
           <strong className="text-gray-400">
@@ -70,10 +70,9 @@ const Experience = () => {
           Segurança de redes, Criptografia, Gestão de riscos, Forense digital,
           Segurança de aplicações.
           <br /> Programação Orientada a Objetos (Java, Python), Banco de dados
-          (Docker e MySQL), Linux e Debian. <br />
-          Habilidades para proteger sistemas, dados e redes contra ameaças
-          cibernéticas, aplicando práticas de segurança, gerenciamento de riscos
-          e conformidade com regulamentações.
+          (Docker e MySQL), Linux e Debian. Habilidades para proteger sistemas,
+          dados e redes contra ameaças cibernéticas, aplicando práticas de
+          segurança, gerenciamento de riscos e conformidade com regulamentações.
         </span>
       ),
       data: "02/2021 - 12/2023 ",
@@ -85,10 +84,10 @@ const Experience = () => {
       subtitulo: "Técnico em Informática para a Internet",
       descricao: (
         <span>
-          Linguagens de Programação (C++, Java, Python, PHP)
-          <br /> Programação Orientada a Objetos (Java, PHP) <br />
-          Linguagens Web (HTML5, CSS3, Javascript), Banco de dados (MySQL),
-          Fundamentos de Hardware e de Redes, Inglês Técnico.
+          Linguagens de Programação (C++, Java, Python, PHP) Programação
+          Orientada a Objetos (Java, PHP) Linguagens Web (HTML5, CSS3,
+          Javascript), Banco de dados (MySQL), Fundamentos de Hardware e de
+          Redes, Inglês Técnico.
         </span>
       ),
       data: "02/2018 - 12/2020 ",
@@ -147,7 +146,7 @@ const Experience = () => {
             </div>
           </div>
         ))}
-        <hr className="my-8 border-t border-gray-300 w-3/4" />
+        <hr className="my-8 border-t border-gray-00 w-3/4" />
         <div className="flex flex-col items-center w-full ">
           <h2 className="text-gray-400 md:text-3xl text-2xl font-bold">
             Educação
@@ -156,7 +155,7 @@ const Experience = () => {
         {formacoes.map((formacoes) => (
           <div
             key={formacoes.id}
-            className={`flex 2xl:flex-row flex-col group transition-all duration-300 gap-5  pb-3 mr-auto rounded-3xl items-start`}
+            className={`flex 2xl:flex-row flex-col  group transition-all duration-300 gap-5  pb-3 mr-auto rounded-3xl items-start`}
           >
             <div className="flex flex-col   items-center justify-start w-full">
               <h2 className="font-bold mx-10 md:w-auto w-36 md:text-3xl text-xl text-gray-300">
