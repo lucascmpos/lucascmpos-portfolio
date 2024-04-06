c23f1431eb9aafa192d67daaa478b1c16d1a96df
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-04-06 11:56:47 -0300

    feat: finishing light mode

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,88 +5,16 @@ import { FaRegCircleUser } from "react-icons/fa6";
 import { MdOutlineWorkOutline } from "react-icons/md";
 import { GoProjectRoadmap } from "react-icons/go";
 import { Link } from "react-scroll";
-import { GiBrazilFlag } from "react-icons/gi";
-import { GiUsaFlag } from "react-icons/gi";
 import { useMediaQuery } from "react-responsive";
 import { IoLanguage } from "react-icons/io5";
 import { FiMoon, FiSun } from "react-icons/fi";
-
-const MobileMenu = ({
-  isOpen,
-  toggleMenu,
-  menuItems,
-  language,
-  toggleLanguage,
-  toggleTheme,
-}) => {
-  return (
-    <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          initial={{ x: "100%" }}
-          animate={{ x: "0%" }}
-          exit={{ x: "100%" }}
-          transition={{ duration: 0.2, ease: "easeInOut" }}
-          className="fixed top-0 right-0 h-full w-full bg-[#020211] text-gray-300 z-20 transition-transform duration-300"
-        >
-          <div className="flex justify-end p-4">
-            <button
-              onClick={() => toggleMenu(false)}
-              className="text-2xl text-gray-300 focus:outline-none"
-            >
-              &#10006;
-            </button>
-          </div>
-          <div className="flex flex-col text-2xl gap-16 items-center pt-10">
-            <Link
-              to="home"
-              smooth={true}
-              duration={500}
-              onClick={() => toggleMenu(false)}
-            >
-              <div>
-                <h2 className="cursor-pointer font-bold text-purple-800">
-                  campos<span className="text-gray-100">.</span>dev
-                </h2>
-              </div>
-            </Link>
-            {menuItems.map((item) => (
-              <Link
-                key={item.id}
-                to={item.id}
-                smooth={true}
-                duration={500}
-                onClick={() => toggleMenu(false)}
-              >
-                <h2 className="flex flex-row cursor-pointer gap-5 justify-center items-center">
-                  <div className="hover:-translate-y-1 hover:text-purple-800">
-                    {item.icon}
-                  </div>
-                  {language === "pt" ? item.text_pt : item.text_en}
-                </h2>
-              </Link>
-            ))}
-          </div>
-          <div className="absolute bottom-10 right-10">
-            <button
-              className="hover:scale-105 flex flex-row items-center justify-center gap-3 hover:text-purple-800 transition-all duration-300"
-              onClick={toggleLanguage}
-            >
-              {language === "pt" ? "EN" : "PT-BR"}
-              <IoLanguage size={23} />
-            </button>
-          </div>
-          <div className="absolute bottom-10 right-10"></div>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
-};
 
 const Header = ({ onChangeLanguage, onChangeTheme }) => {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [language, setLanguage] = useState("pt");
   const [theme, setTheme] = useState("dark");
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
 
   const toggleMenu = () => {
@@ -112,6 +40,17 @@ const Header = ({ onChangeLanguage, onChangeTheme }) => {
       document.body.style.overflow = "auto";
     }
   }, [isMenuOpen]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible]);
 
   const menuItems = [
     {
@@ -140,12 +79,122 @@ const Header = ({ onChangeLanguage, onChangeTheme }) => {
     },
   ];
 
+  const MobileMenu = ({
+    isOpen,
+    toggleMenu,
+    menuItems,
+    language,
+    toggleLanguage,
+    toggleTheme,
+  }) => {
+    return (
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: "0%" }}
+            exit={{ x: "100%" }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+            className="fixed top-0 right-0 h-full w-full  text-gray-300 z-20 transition-transform duration-300 backdrop-filter backdrop-blur-3xl"
+          >
+            <div className="flex justify-end p-4">
+              <button
+                onClick={() => toggleMenu(false)}
+                className={`text-2xl  focus:outline-none ${
+                  theme === "light" ? "text-black" : "text-gray-300"
+                }`}
+              >
+                &#10006;
+              </button>
+            </div>
+            <div className="flex flex-col text-2xl gap-16 items-center pt-10">
+              <Link
+                to="home"
+                smooth={true}
+                duration={500}
+                onClick={() => toggleMenu(false)}
+              >
+                <div>
+                  <h2 className="cursor-pointer font-bold text-purple-800">
+                    campos
+                    <span
+                      className={`${
+                        theme === "light" ? "text-black" : "text-gray-200"
+                      }`}
+                    >
+                      .
+                    </span>
+                    dev
+                  </h2>
+                </div>
+              </Link>
+              {menuItems.map((item) => (
+                <Link
+                  key={item.id}
+                  to={item.id}
+                  smooth={true}
+                  duration={500}
+                  onClick={() => toggleMenu(false)}
+                >
+                  <h2
+                    className={`flex flex-row cursor-pointer gap-5 justify-center items-center ${
+                      theme === "light" ? "text-black" : "text-gray-200"
+                    }`}
+                  >
+                    <div className="hover:-translate-y-1 hover:text-purple-800">
+                      {item.icon}
+                    </div>
+                    {language === "pt" ? item.text_pt : item.text_en}
+                  </h2>
+                </Link>
+              ))}
+            </div>
+            <div className="absolute bottom-10 right-10">
+              <button
+                className={`hover:scale-105 flex flex-row items-center justify-center gap-3 hover:text-purple-800 transition-all duration-300 ${
+                  theme == "light" ? "text-black" : "text-gray-200"
+                }`}
+                onClick={toggleLanguage}
+              >
+                {language === "pt" ? "EN" : "PT-BR"}
+                <IoLanguage size={23} />
+              </button>
+            </div>
+            <div className="absolute bottom-10 right-10"></div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    );
+  };
+
   return (
-    <header className="fixed  flex flex-row text-lg md:justify-around justify-between items-center bg-[#020211] text-gray-300 font-semibold w-full p-5 z-10 shadow-md">
+    <header
+      className={`fixed flex flex-row text-lg md:justify-around justify-between items-center  font-semibold w-full p-5 z-10 shadow-md transition-opacity duration-300 ${
+        !visible
+          ? "opacity-0 pointer-events-none"
+          : "opacity-100 pointer-events-auto"
+      } ${
+        theme === "light"
+          ? "bg-gray-300 text-black"
+          : "bg-[#020211] text-gray-300"
+      }`}
+    >
       <Link to="home" smooth={true} duration={500}>
         <div>
-          <h2 className="cursor-pointer font-bold text-purple-800">
-            campos<span className="text-gray-100">.</span>dev
+          <h2
+            className={`cursor-pointer font-bold ${
+              theme === "light" ? "text-purple-600" : "text-purple-800"
+            } `}
+          >
+            campos
+            <span
+              className={`${
+                theme === "light" ? "text-black" : "text-gray-200"
+              }`}
+            >
+              .
+            </span>
+            dev
           </h2>
         </div>
       </Link>
@@ -181,7 +230,9 @@ const Header = ({ onChangeLanguage, onChangeTheme }) => {
 
         <button
           onClick={toggleMenu}
-          className="md:hidden text-2xl text-gray-300 focus:outline-none"
+          className={`md:hidden text-2xl  focus:outline-none ${
+            theme === "light" ? "text-black" : "text-gray-300"
+          }`}
         >
           &#9776;
         </button>
